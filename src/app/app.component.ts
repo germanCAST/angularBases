90866cc178ad83e237663ff6188f141fcc6123bf
German <germancastellanose@gmail.com>
2024-03-06 19:16:30 -0600

    biding unidireccional no stand

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Hola mundo, soy German';
+  public counter: number = 10;
+
+  operacion(valor: number) {
+    this.counter = this.counter + valor;
+  }
 }
