fc5fcae62a4e3cbad4cb2f79224e10a677e95f75
German <germancastellanose@gmail.com>
2024-03-13 05:23:26 -0600

    @input y NgFOr con Index

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-main-page',
   templateUrl: 'main-page.component.html',
 })
-export class MainPageComponent {}
+export class MainPageComponent {
+  public characters: Character[] = [
+    { name: 'Goku', power: 9000 },
+    { name: 'Krillin', power: 3000 },
+    { name: 'Trunks jr', power: 4000 },
+  ];
+}
